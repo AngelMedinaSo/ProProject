cdd9904c4e9b2154e6f0e768e38b88ecd8068b83
AngelMedinaSo <9030149@student.tcrmbo.nl>
2026-06-16 14:10:52 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$appNaam = "ProProject";
+$trackerType = "Scrimtracker";
+$tagline = "Review. Improve. Dominate.";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $appNaam; ?></title>
+</head>
+<body>
+
+    <h1><?php echo $appNaam; ?></h1>
+
+</body>
+</html>
